045a37e095fbcf6eabffa7c2e2a2646a5471a79a
R Ramana <54029207+R-Ramana@users.noreply.github.com>
2021-04-01 21:54:02 +0800

    Update iir.s

--- workspace/CG2028AssignS2AY2021/src/iir.s
+++ workspace/CG2028AssignS2AY2021/src/iir.s
@@ -13,23 +13,142 @@
 @R1 - b
 @R2 - a
 @R3 - x_n
-@R4 - <use(s)>
-@R5 - <use(s)>
-@....
+@R4 - x_store address
+@R5 - y_store address
+@R6 - x_store tracking pointer
+@R7 - y_store tracking pointer
+@R8 - a, b values
+@R9 - x_store, y_store values
+@R10 - N counter
+@R11 - y_n
+
 
 iir:
 @ PUSH / save (only those) registers which are modified by your function
 @ parameter registers need not be saved.
-
+	PUSH {R4-R11}
 @ write asm function body here
+	@ R10 is N-counter, number of times to run the loop/function
+	@ Ensures R0 is not modified
+	MOV R10, R0
+
+	@ b[0], post-indexing offset to get b1 value the next call
+	LDR R8, [R1], #4
+	
+	@ b[0] * x_n
+	MUL R11, R3, R8
+
+	@ load start address of the static array to x_store, y_store
+	LDR R4, =x_store
+	LDR R5, =y_store
+	
+	@ load pointer to (n-1)th values of x and y
+	LDR R6, [R4]
+	
+	@ check if address exists in x_store yet
+	@CMP R6, #0
+	@BEQ zero
+
+	@ increment to reference a[1]
+	@ R2 is the address of a values
+	@ divide by a[0] at the end
+	ADD R2, #4
+
+	LDR R7, [R5]
+	@ update x_store, y_store first 4 bytes to their own address
+	@ as a flag for the loop
+	STR R4, [R4]
+	STR R5, [R5]
+
+loop:
+
+	@ load x[n-i] value
+	LDR R9, [R6], #-4
+	@ check if value loaded is the start address
+	CMP R9, R4
+	BEQ circle
+
+
+	@ b[i]
+	LDR R8, [R1], #4
+
+	@ y_n += b[i] * x[n-i]
+	MLA R11, R8, R9, R11
+
+	@ load y[n-i] value
+	LDR R9, [R7], #-4
+
+	@ a[i]
+	LDR R8, [R2], #4
+
+	@ y_n -= a[i] * y[n-i]
+	MLS R11, R8, R9, R11
+
+	@ decrement counter
+	SUB R10, #1
+	CMP R10, #0
+	BGT loop
+	B end
+
+@ load end addresses of x_store, y_store
+@ N bytes from start addresses
+circle:
+	@ LSL 2 = multiply by 4, so N_Max * 4 number of bytes for x
+	ADD R6, R4, R0, LSL #2
+	
+	@ LSL 2 = multiply by 4, so N_Max * 4 number of bytes for y
+	ADD R7, R5, R0, LSL #2
+	
+	B loop
+
+@ for x_0, y_0 no address yet in x_store, y_store
+@ get address
+zero:
+	MOV R6, R4
+	MOV R7, R5
+	@ just so that i = 0 is consistent
+	ADD R2, R2, R0, LSL #2
+	ADD R2, #4
+
+end:
+	@ get back start address of a to get a1
+	@ R0, LSL 2 = N * 4
+	SUB R2, R2, R0, LSL #2
+	
+	@ to get start address a0
+	SUB R2, #4
+	LDR R8, [R2]
+
+	@ /a[0]
+	SDIV R11, R8
+
+	@ store x_n, y_n into x_store, y_store
+	STR R3, [R6, #4]!
+	STR R11, [R7, #4]!
+
+	@ store pointer to (n-1)th address for next n
+	STR R6, [R4]
+	STR R7, [R5]
+
+	@ /100 for scaling
+	MOV R10, #100
+	@SDIV R11, R10
+	MOV R0, R11
+
+
 
 @ prepare value to return (y_n) to C program in R0
 
 @ POP / restore original register values. DO NOT save or restore R0. Why?
-
+	POP {R4-R11}
 @ return to C program
 		BX	LR
 
 @label: .word value
 .equ N_MAX, 10
 @.lcomm label num_bytes
+@ each int = 32 bits = 4 bytes
+@ store at most N_MAX integers = 4 * N_MAX (10) bytes
+@ the first 4 bytes stores a pointer to (n-1)th value
+.lcomm x_store, 44
+.lcomm y_store, 44
